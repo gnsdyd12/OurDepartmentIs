c058f627855ef39c546e30e7fa8a74b6e17a9da0
LimEunSang <dmstkd2905@naver.com>
2022-06-07 17:28:11 +0900

    frontend 좋아요 버튼 생성
    - frontend에서 좋아요 버튼을 구현했습니다. 좋아요 상태이면 속이 찬 하트, 그렇지 않으면 속이 빈 하트를 출력합니다.

--- frontend/src/pages/ViewPost.js
+++ frontend/src/pages/ViewPost.js
@@ -4,9 +4,32 @@ import { LoginInfoContext } from "../App";
 import axios from "axios";
 import "@toast-ui/editor/dist/toastui-editor-viewer.css";
 import { Viewer } from "@toast-ui/react-editor";
-import { Container, Typography, Box, Stack, Button } from "@mui/material";
+import {
+  Container,
+  Typography,
+  Box,
+  Stack,
+  Button,
+  IconButton,
+  useMediaQuery,
+} from "@mui/material";
 import { basicColor } from "../color";
 import { API_BASE_URL } from "../URL";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import { createTheme } from "@mui/system";
+import { ThemeProvider } from "@emotion/react";
+
+const theme = createTheme({
+  breakpoints: {
+    values: {
+      mobile: 0,
+      tablet: 640,
+      laptop: 1024,
+      desktop: 1200,
+    },
+  },
+});
 
 const ViewPost = () => {
   // 게시글의 id 값을 url로부터 저장
@@ -38,8 +61,40 @@ const ViewPost = () => {
       });
   }, []);
 
+  const heartButtonActive = useMediaQuery("(min-width: 1440px)");
+
+  // 좋아요 상태를 관리할 변수
+  const [isHeart, setIsHeart] = useState(false);
+
   return (
     <>
+      {/* 좋아요 버튼 */}
+      {heartButtonActive && (
+        <Box
+          sx={{
+            position: "fixed",
+            top: "20%",
+            left: "10%",
+            border: "1px solid",
+            borderRadius: 8,
+            py: "14px",
+            px: "4px",
+          }}
+        >
+          <IconButton
+            sx={{ border: "solid 1px" }}
+            onClick={() => setIsHeart(!isHeart)}
+          >
+            {isHeart === false ? (
+              <FavoriteBorderIcon sx={{ fontSize: "2rem" }} />
+            ) : (
+              <FavoriteIcon sx={{ fontSize: "2rem" }} />
+            )}
+          </IconButton>
+          <Typography textAlign="center">100</Typography>
+        </Box>
+      )}
+
       <Container
         maxWidth="md"
         sx={{
