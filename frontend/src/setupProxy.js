ad6249937840caf7d07c269739453f6c5cf0c720
LimEunSang <dmstkd2905@naver.com>
2023-08-29 16:54:48 +0900

    도메인 주소 적용
    - public ip 주소를 등록한 도메인 주소로 변경하였습니다.
    - 바로 전 push 에서 빼먹은 부분을 마저 수정했습니다.

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,8 +4,10 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://3.36.128.239:8080",
       // target: "http://localhost:8080",
+      // target: "http://3.36.128.239:8080",
+      target: "http://ourdepartmentis.o-r.kr:8080",
+
       changeOrigin: true,
     })
   );
