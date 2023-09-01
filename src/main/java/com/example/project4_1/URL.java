d108b926d3999fc245d9b9a53bff927c8fdcc26b
LimEunSang <dmstkd2905@naver.com>
2023-09-01 15:05:12 +0900

    잘못 변경되어있던 URL 수정
    과거에 localhost로 되어있던 주소를 잘못 변경하였는지,
    공개 서버 ip 주소로 작성된 주소를 올바르게 수정했습니다.

--- src/main/java/com/example/project4_1/URL.java
+++ src/main/java/com/example/project4_1/URL.java
@@ -2,8 +2,8 @@ package com.example.project4_1;
 
 public class URL {
 
-    private String API_BASE_URL = "http://3.36.128.239:3000";
-//    private String API_BASE_URL = "http://localhost:3000";
+    private String API_BASE_URL = "http://localhost:3000";
+//    private String API_BASE_URL = "http://ourdepartmentis.site";
 
     public String getAPI_BASE_URL() {
         return API_BASE_URL;
